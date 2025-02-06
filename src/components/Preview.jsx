8d23e2382c37c06c1e3f9673863b406a9b7d1f27
Meredith <meredithflss@gmail.com>
2025-02-06 13:40:44 +1300

    table restyle

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import testItem from '/public/data/testItem.json';
 import { MdMoneyOff, MdAttachMoney } from "react-icons/md";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import ScrollTop from './ScrollTop';
+import { HiOutlineBadgeCheck } from "react-icons/hi";
+
+// import ScrollTop from './ScrollTop';
 // import PrimaryButton from './PrimaryButton';
 
 const Preview = () => {
@@ -38,35 +40,36 @@ const Preview = () => {
     return (
         <>
             <section className='populated-items'>
-                {currentItems.map((item, index) => (
-                    <Link
-                        key={index}
-                        to={`/item/${index}`}
-                        className={`item item--${index + 1}`}
-                    >
-                        <div className='row'>
-                            <h2>{item.Name}</h2>
-                            <div className='details'>
-                                <h3 className={`${item.Details.Free === 'Yes' ? 'free' : 'hidden'}`}><MdMoneyOff /></h3>
-                                <h3 className={`${item.Details.Free === 'No' ? 'paid' : 'hidden'}`}><MdAttachMoney /></h3>
-                            </div>
-                        </div>
-                        <div className='row item-capabilities'>
-                            {item.Details.Capabilities.slice(0, 5).map((tech, index) => (
-                            <span key={index} className="item-capabilities-icons">
-                                {tech}
-                            </span>
-                            ))}
-                            {item.Details.Capabilities.length > 5 && (
-                                <span className="item-capabilities-icons more">...</span>
-                            )}
-                        </div>
-                        <div className='row'>
-                            <h6>{item.Author}</h6>
-                            <h6>{item.LastModified}</h6>
-                        </div>
-                    </Link>
-                ))}
+                <div className='populated-items--grid--cont'>
+                    <table className='rwd-table'>
+                        <tr>
+                            <th>Name</th>
+                            <th>Type</th>
+                            {/* <th>Capabilities</th> */}
+                            <th>Last Updated</th>
+                            <th>Verified</th>
+                            <th>Cost</th>
+                        </tr>
+
+                        {currentItems.map((item, index) => (
+                        <tr key={index} className="clickable-row">
+                            <Link to={`/item/${index}`} className="row-link">
+                                <td data-th="Movie Title" className='item-logo'>
+                                    <img src={item.Logo}/>
+                                    <h4>{item.Name}</h4>
+                                </td>
+                                <td data-th="Genre">{item.categoryType}</td>
+                                <td data-th="Gross">{item.LastModified}</td>
+                                <td data-th="Gross" className='verification-icon'><HiOutlineBadgeCheck className='verification-icon'/></td>
+                                <td data-th="Gross">
+                                    <h4 className={`${item.Details.Free === 'Yes' ? 'free' : 'hidden'}`}><MdMoneyOff /></h4>
+                                    <h4 className={`${item.Details.Free === 'No' ? 'paid' : 'hidden'}`}><MdAttachMoney /></h4>
+                                </td>
+                            </Link>
+                        </tr>
+                        ))}
+                    </table>
+                </div>
             </section>
 
             <div className="pagination">
